fb2b485a48204d9ca82b4e785228f0acdcf9706d
pe5ha <56196846+pe5ha@users.noreply.github.com>
2021-10-27 20:46:45 +0300

    delete token

--- src/main/java/SkiffLadderBot.java
+++ src/main/java/SkiffLadderBot.java
@@ -10,8 +10,7 @@ import java.util.*;
 
 public class SkiffLadderBot extends TelegramLongPollingBot {
     String name="@SkiffLadderGameBot";
-//    String token="1749369333:AAEQ2ikfg0NWghbHUqvrOXfa4Z0I2J3TZwo";
-    String token="1796068046:AAGqlRdhy-XDaV73PtHyzR6DL9LjIR7M1iA";
+    String token="";
     private final String BOT_TOKEN = "" + (System.getenv("BOT_TOKEN") == null ? token : System.getenv("BOT_TOKEN"));
     DatabaseController database = new DatabaseController();
     GameController gameController = new GameController();
